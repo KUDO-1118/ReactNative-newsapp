3312ccdd0b35f9cb4f20f056cf75bd1eb52d9f04
{KUDO} <au4saf010@gmail.com>
2024-10-13 14:47:38 +0900

    news-app差分

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, View, Text } from 'react-native';
+import { ListItem } from './components/ListItem';
+import articles from "./dummies/articles.json";
 
 export default function App() {
+  const items = articles.map((article, index) => {
+    return(
+      <ListItem
+        imageUrl={article.urlToImage}
+        title={article.title}
+        author={article.author}
+        ket={index.toString()}
+      />)
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      { items }
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +25,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eee',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  itemContainer: {
+    height: 100,
+    width: "100%",
+    backgroundColor: "white",
+    flexDirection: "row",
+    marginVertical: 5,
+  },
+  leftContainer: {
+    width: 100
+  },
+  rightContainer: {
+    flex: 1,
+    padding: 10,
+    justifyContent: "space-between",
+  },
+  text: {
+    fontSize: 16,
+  },
+  subtext: {
+    fontSize: 12,
+    color: "gray"
+  }
+
 });
---
+++ components/ListItem.js
@@ -0,0 +1,56 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Image, View, Text } from 'react-native';
+
+/**
+ *
+ * @param {
+ *   imageUrl: 画像URL(string)
+ *   title: タイトル(string)
+ *   author: ニュース提供元(string)'https://reactnative.dev/img/tiny_logo.png'
+ * } props
+ * @returns
+ */
+
+export const ListItem = (props) => {
+  return (
+      <View style={styles.itemContainer}>
+        <View style={styles.leftContainer}>
+          <Image
+            style={{ width: 100, height: 100 }}
+            source={{ url: props.imageUrl }}
+          />
+        </View>
+        <View style={styles.rightContainer}>
+          <Text numberOfLines={3} style={styles.text}>
+            {props.title }
+          </Text>
+        <Text style={styles.subtext}>{props.author}</Text>
+        </View>
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    height: 100,
+    width: "100%",
+    backgroundColor: "white",
+    flexDirection: "row",
+  },
+  leftContainer: {
+    width: 100
+  },
+  rightContainer: {
+    flex: 1,
+    padding: 10,
+    justifyContent: "space-between",
+  },
+  text: {
+    fontSize: 16,
+  },
+  subtext: {
+    fontSize: 12,
+    color: "gray"
+  }
+
+});

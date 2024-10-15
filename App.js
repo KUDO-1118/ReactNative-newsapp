464d7e3e00b02d65e198516d7af83b52d8feb99e
{KUDO} <au4saf010@gmail.com>
2024-10-15 12:07:03 +0900

    Bottom Tab Navigatorで画面下にタブアイコンを表示する

--- App.js
+++ App.js
@@ -1,18 +1,46 @@
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesome } from "@expo/vector-icons"
 import { HomeScreen } from './screens/HomeScreen';
 import { ArticleScreen } from './screens/ArticleScreen';
+import { ClipScreen } from './screens/ClipScreen';
 
+const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
+
+const screenOption = ({ route }) => ({
+  tabBarIcon: ({ color, size }) => {
+    let iconName;
+
+    if (route.name === 'HomeTab') {
+      return <FontAwesome name='home' size={size} color={color} />
+    } else if (route.name === 'ClipTab') {
+      return <FontAwesome name='bookmark' size={size} color={color} />
+    }
+
+    // You can return any component that you like here!
+    return <Ionicons name={iconName} size={size} color={color} />;
+  },
+})
+
+const HomeStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="News Index" component={HomeScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="Article" component={ArticleScreen} />
+    </Stack.Navigator>
+  )
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="News Index" component={HomeScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Article" component={ArticleScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator screenOptions={screenOption}>
+        <Tab.Screen name="HomeTab" component={HomeStack} options={{ headerShown: false, title: "Home" }}/>
+        <Tab.Screen name="ClipTab" component={ClipScreen} options={{ headerShown: false, title: "Clip" }}/>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, SafeAreaView, Text } from "react-native";
+import { StyleSheet, SafeAreaView } from "react-native";
 import { WebView } from 'react-native-webview';
 
 export const ArticleScreen = ({ route }) => {
@@ -6,6 +6,7 @@ export const ArticleScreen = ({ route }) => {
   return (
     <SafeAreaView style={styles.container}>
       <WebView
+      // originWhitelist={['*']}
       source={{ uri: article.url }}
     />
     </SafeAreaView>

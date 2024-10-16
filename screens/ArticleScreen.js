83746d0c9cd973f3bbfc996440b7ef3725d0bd8a
{KUDO} <au4saf010@gmail.com>
2024-10-16 10:33:29 +0900

    クリップ一覧画面の作成

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,19 +1,25 @@
 import { StyleSheet, SafeAreaView } from "react-native";
 import { WebView } from 'react-native-webview';
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { ClipButton } from "../components/ClipButton";
-import { addClip } from "../store/userSlice";
+import { addClip, deleteClip } from "../store/userSlice";
 
 export const ArticleScreen = ({ route }) => {
   const { article } = route.params;
   const dispatch = useDispatch();
+  const clips = useSelector((state) => state.user.clips)
+  const isClipped = clips.some((clip) => clip.url === article.url)
 
   const onPressClip = () => {
-    dispatch(addClip(article))
-  }
+    if (isClipped) {
+      dispatch(deleteClip(article));
+    } else {
+      dispatch(addClip(article));
+    }
+  };
   return (
     <SafeAreaView style={styles.container}>
-      <ClipButton onPress={onPressClip} enabled={false} />
+      <ClipButton onPress={onPressClip} enabled={isClipped} />
       <WebView
       source={{ uri: article.url }}
     />

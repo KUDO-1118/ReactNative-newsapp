5ae5ffc64de1abf99844845cd49c397056eef8f9
{KUDO} <au4saf010@gmail.com>
2024-10-16 11:08:00 +0900

    redux-persistでreduxの状態を永続化する

--- store/index.js
+++ store/index.js
@@ -1,8 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from '@reduxjs/toolkit';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { persistStore } from 'redux-persist';
 import  userReducer from './userSlice'
+import persistReducer from 'redux-persist/es/persistReducer';
+import reducer from './userSlice';
+
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
-})
+    reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
